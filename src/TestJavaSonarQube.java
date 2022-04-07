5a5a77e93b4ee348817be0289a861364d9b9cb07
zhuwenhao <zhuwenhao@fulan.com.cn>
2022-04-07 19:45:15 +0800

    test 提交sonarqube测试

--- src/TestJavaSonarQube.java
+++ src/TestJavaSonarQube.java
@@ -6,4 +6,12 @@ public class TestJavaSonarQube {
         target =- num;  // Noncompliant; target = -3. Is that really what's meant?
         target =+ num; // Noncompliant; target = 3
     }
+    public void aa(String a,String b){
+        if(1==1){
+            System.out.println(a);
+        }
+        if(a.equals("bbb")){
+            System.out.println(b);
+        }
+    }
 }
