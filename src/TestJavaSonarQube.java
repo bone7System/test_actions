abfc99393f11a28883ac5881c89f42df4a90745c
zhuwenhao <zhuwenhao@fulan.com.cn>
2022-04-13 16:47:09 +0800

    更新

--- src/TestJavaSonarQube.java
+++
@@ -1,9 +0,0 @@
-public class TestJavaSonarQube {
-    public static void main(String[] args) {
-        int target = -5;
-        int num = 3;
-
-        target =- num;  // Noncompliant; target = -3. Is that really what's meant?
-        target =+ num; // Noncompliant; target = 3
-    }
-}
