d3dfa3b7f6887a226f6418f1c77b64ba9cd78501
zhuwenhao <zhuwenhao@fulan.com.cn>
2022-04-13 17:04:28 +0800

    更新

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -6,8 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo1Application {
 
+    dddddddd
+    String password="aaabbcc";
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
+    }
+
+    public void sgt(){
+        while (true){
+            System.out.println(123);
+
+        }
+        if(1==1){
+            System.out.println("aaaa");
+        }
     }
 
 }
